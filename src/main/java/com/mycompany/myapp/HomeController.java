38c16f790824ec9111d7ed3cca74cb8e6ac29d23
Simzu <ams2009@naver.com>
2018-08-29 15:27:19 +0900

    setting spring tiles switching bootstrap version to 3.3.7

--- src/main/java/com/mycompany/myapp/HomeController.java
+++ src/main/java/com/mycompany/myapp/HomeController.java
@@ -4,8 +4,6 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,23 +15,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
+    public String home(Locale locale, Model model) {
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        String formattedDate = dateFormat.format(date);
+        model.addAttribute("serverTime", formattedDate);
+        // 기존 "home.jsp"에서 변경
+        // */* 형태로 페이지를 요청하였으므로,
+        // tiles.xml 설정에 의해 content로 전송 됨.
+        return "static/home";
+    }
 	
 }
---
+++ src/main/webapp/WEB-INF/views/static/home.jsp
@@ -0,0 +1,5 @@
+
+<div class="jumbotron">
+	<h1>Hello world!</h1>
+	<p>The time on the server is ${serverTime}.</p>
+</div>
